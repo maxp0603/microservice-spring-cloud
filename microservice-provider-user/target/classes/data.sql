796dac861003d99ed40cbd625f72c67234327bb4
maxp <maxp0603@163.com>
2019-02-27 18:39:07 +0800

    delete target

--- microservice-provider-user/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user(id,username, name, age) values(1,'user1', '张三', 20);
-insert into user(id,username, name, age) values(2,'user2', '李四', 20);
-insert into user(id,username, name, age) values(3,'user3', '王五', 20);
-insert into user(id,username, name, age) values(4,'user4', '马六', 20);
