ee8da99eedf0c60c7a31d4422eba89e10fa5dbe0
maxp <maxp0603@163.com>
2019-03-01 16:23:10 +0800

    使用Feign实现声明式REST调用

---
+++ microservice-consumer-movie-feign/src/main/java/com/huaxin/microserviceconsumermovie/ConsumerMovieFeignApplication.java
@@ -0,0 +1,17 @@
+package com.huaxin.microserviceconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@EnableEurekaClient
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumerMovieFeignApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMovieFeignApplication.class, args);
+	}
+
+}
---
+++ microservice-consumer-movie-feign/src/main/java/com/huaxin/microserviceconsumermovie/controller/MovieController.java
@@ -0,0 +1,18 @@
+package com.huaxin.microserviceconsumermovie.controller;
+
+import com.huaxin.microserviceconsumermovie.domain.User;
+import com.huaxin.microserviceconsumermovie.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id){
+        return this.userFeignClient.findById(id);
+    }
+}
